197c1fdaeeef6768565e70c4dee1056a570bdacd
laithalawad745 <laithalawad745@gmail.com>
2023-03-08 00:26:00 +0300

    ccc

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,16 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+   let req = new XMLHttpRequest();
+
+   req.onreadystatechange = function () {
+     if (this.readyState === 4 && this.status === 200) {
+        console.log(this.responseText)
+    //    let b = JSON.parse(t);
+    //    console.log(b[4].name);
+     }
+   };
+   req.open("GET", "https://localhost:7219/api/APIconteroller", true);
+   req.send();
